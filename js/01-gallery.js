ea936d204954a31bea0f4f526017b166a7af0f96
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-03-26 22:31:49 +0300

    create-task-01-and-02

---
+++ js/01-gallery.js
@@ -0,0 +1,63 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+console.log(createImagesCards(galleryItems));
+const imagesContainer = document.querySelector('.gallery');
+//const imagesEl = document.querySelector('.gallery__item');
+const cardsMarkup = createImagesCards(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onContainerClick);
+function createImagesCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    }).join('');
+
+}
+
+
+
+
+function onContainerClick(evt) {
+    evt.preventDefault();
+    const isImageGallery = evt.target.classList.contains('gallery__image');
+    if (!isImageGallery) {
+        return;
+    }
+    const activeEvtUrl = evt.target.dataset.source;
+    console.log(activeEvtUrl);
+
+    //import * as basicLightbox from 'basiclightbox'
+ 
+    const instance = basicLightbox.create(`<div class="modal">
+       <img src="${activeEvtUrl}" width="1280" height="auto"/>
+    </div>`,
+        {
+              onShow: (instance) => {
+                instance.element().querySelector('img').onclick = instance.close;
+            },
+        }    
+    );
+    instance.show();
+    document.addEventListener('keydown', onEscKey);
+    function onEscKey(evt) {
+        if (evt.key === 'Escape') {
+            instance.close();
+        }
+    }
+ }
+
+
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,22 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+//console.log(galleryItems);
+//console.log(createImagesCards(galleryItems));
+const imagesContainer = document.querySelector('.gallery');
+//const imagesEl = document.querySelector('.gallery__item');
+const cardsMarkup = createImagesCards(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onContainerClick);
+function createImagesCards(galleryItems) {
+ return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    }).join('');
+    
+};
